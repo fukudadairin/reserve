817b85341cdd20e3e61032025c3ef0a880510914
fukudadairin <79754701+fukudadairin@users.noreply.github.com>
2022-08-03 08:08:07 +0900

    UI設計中、confirm,phpにCSSが反映されない

--- confirm.php
+++ confirm.php
@@ -1,1 +1,77 @@
-予約内容の確認
+<!-- 予約内容の確認 -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    header("Location:/reserve/confirm.php/");
+}
+
+?>
+
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- original CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>予約内容確認</title>
+</head>
+
+<body>
+    <header>SAMPLE　SHOP</header>
+    <h1>予約内容確認</h1>
+
+    <table class="table">
+
+        <tbody>
+            <tr>
+                <th scope="row">日時</th>
+                <td>2022年07月30日(土)17時00分</td>
+            </tr>
+            <tr>
+                <th scope="row">人数</th>
+                <td>4名</td>
+            </tr>
+            <tr>
+                <th scope="row">氏名</th>
+                <td colspan="2">田中　ロドリゲス　太郎</td>
+            </tr>
+            <tr>
+                <th scope="row">メールアドレス</th>
+                <td colspan="2">test@test</td>
+            </tr>
+            <tr>
+                <th scope="row">電話番号</th>
+                <td colspan="2">1111111111</td>
+            </tr>
+            <tr>
+                <th scope="row">備考</th>
+                <td colspan="2">備考の内容が入る備考の内容が入る備考の内容が入る備考の内容が入る備考の内容が入る</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <div class="d-grid gap-2">
+        <button class="btn btn-primary rounded-pill" type="submit">予約確認</button>
+        <button class="btn btn-secondary rounded-pill" type="button">戻る</button>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
